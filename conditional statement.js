cb80e447f13586e8c2cc448c94ca528c09de56e2
Shreyash <shreyashdeulkar8@gmail.com>
2025-07-26 20:24:50 +0530

    operators

---
+++ conditional statement.js
@@ -0,0 +1,18 @@
+// conditional statement
+
+let mode="dark";
+let color;
+if (mode==="dark"){
+    color="black";
+}else if (mode=="blue"){
+    color="blue";
+}else if(mode=="pink"){
+color="pink";
+}else{
+    color="white";
+}
+console.log(color);
+
+
+
+
