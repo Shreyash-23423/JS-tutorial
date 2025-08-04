dc311cdfdaf811f10d555b6c9ad019855cff4ae9
Shreyash <shreyashdeulkar8@gmail.com>
2025-08-04 23:38:40 +0530

    Clean up old scripts and add practice & tutorial files

---
+++ practicequestios.js
@@ -0,0 +1,6 @@
+let number = prompt("Enter your Number")
+if(number%5== 0 ){
+    console.log("Number is divisible by 5")
+}else{
+    console.log("Number is not divisible by 5")
+}
---
+++ scriptstutoial.js
@@ -0,0 +1,87 @@
+ // global variables
+// // arithmetic operators
+// let a = 10;
+// let b = 2;
+// console.log("a+b=", a + b);
+// console.log("a-b=", a - b);
+// console.log("a*b=", a * b);
+// console.log("a/b=", a / b);
+// console.log("a%b=", a % b);
+// console.log("a**b=", a ** b);
+
+
+// unary operators
+// console.log("a=", a, "b=", b);
+// a++;
+// console.log("a=", a);
+
+// assignment operators
+// a += 4;
+// console.log("a=", a);
+
+// comparison operator
+// console.log("a==b", a == b);
+// console.log("a>b", a > b);
+
+
+// // logical operator
+
+// let cond1 = a > b;
+// let cond2 = a == 10;
+// console.log("cond1 && cond2", cond1 && cond2);
+// console.log("cond1 || cond2", cond1 || cond2);
+// console.log("!(a<b)", !(a < b));
+
+
+
+
+
+
+//  // //conditional statement// // //
+
+// // Conditional statement example
+
+// let age = 5;
+// if (age >= 18) {
+//     console.log("eligible for licence");
+// } else {
+//     console.log("not eligible for licence");
+// }
+
+// let mode = "dark";
+// let color;
+// if (mode == "dark") {
+//     color = "black";
+// } else {
+//     color = "white";
+// }
+// console.log(color);
+
+// // Check if a number is odd or even
+// let num = 10;
+// if (num % 2 == 0) {
+//     console.log("num is even");
+// } else {
+//     console.log("num is odd");
+// }
+
+
+// if (mode==="dark"){
+//     color="black";
+// }else if (mode=="blue"){
+//     color="blue";
+// }else if(mode=="pink"){
+// color="pink";
+// }else{
+//     color="white";
+// }
+// console.log(color);
+
+
+// // // Ternary operator example
+// let age = 18;
+// age >= 18 ? console.log("Yes") : console.log("No");
+
+// ternary operator
+
+// age >= 18 ? console.log("Yes") : console.log("No");
